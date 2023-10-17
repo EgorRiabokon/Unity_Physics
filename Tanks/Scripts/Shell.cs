0254efcb97a7e0c7193a255d236f8e83493a90cc
EgorRiabokon <76178238+EgorRiabokon@users.noreply.github.com>
2023-10-17 20:31:01 +0300

    Add files via upload

---
+++ Tanks/Scripts/Shell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+    private float speed = 0f;
+    private float yspeed = 0f;
+    private float mass = 10f;
+    private float force = 2f;
+    private float drag = 0.3f;
+    private float gravity = -9.8f;
+    private float gAccel;
+    private float acceleration;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceleration = force / mass;
+        speed += acceleration * 1;
+        gAccel = gravity / mass;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        speed *= (1 - Time.deltaTime * drag);
+        yspeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0, yspeed, speed * Time.deltaTime);
+    }
+}
